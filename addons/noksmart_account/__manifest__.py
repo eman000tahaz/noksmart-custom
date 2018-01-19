ac5b268c57f45824638b0bd1e915ec7392d7d993
Eman taha <emeytahaz@gmail.com>
2018-01-20 01:51:49 +0200

    [ADD] accounting module

---
+++ addons/noksmart_account/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Noksmart Accounting',
+    'version': '1.0.1',
+    'Author': "Eman Taha",
+    'category': 'Accounting',
+    'sequence': 20,
+    'summary': 'Noksmart Accounting',
+    'depends': ['account'],
+    'data': [
+        'views/account_views.xml',
+    ],
+    'demo': [
+    ],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+}
---
+++ addons/noksmart_account/models/account_account.py
@@ -0,0 +1,40 @@
+from odoo import api, fields, models, _
+
+
+class AccountAccountInherit(models.Model):
+    _inherit = 'account.account'
+
+    parent_id = fields.Many2one('account.account', 'Parent Account')
+    child_ids = fields.One2many('account.account', 'parent_id', 'Children')
+    code = fields.Char(string='Code', required=False, help="The journal entries of this journal will be named using this prefix.")
+
+
+    @api.model
+    def create(self, vals):
+    	if vals['parent_id'] != False:
+    		parent_acc = self.env['account.account'].search([('id', '=', vals['parent_id'])])
+    		if parent_acc.parent_id:
+    			if parent_acc.child_ids:
+    				ids=[]
+    				for child in parent_acc.child_ids:
+    					ids.append(child.id)
+    				last_child_acc = self.env['account.account'].search([('id', 'in', ids)], order='id desc', limit=1)
+    				vals['code'] = str(int(last_child_acc.code)+1)	
+    			else:
+    				vals['code'] = parent_acc.code + "00" +str(1)
+    		else:
+    			if parent_acc.child_ids:
+    				ids=[]
+    				for child in parent_acc.child_ids:
+    					ids.append(child.id)
+    				last_child_acc = self.env['account.account'].search([('id', 'in', ids)], order='id desc', limit=1)
+    				vals['code'] = str(int(last_child_acc.code)+1000)	
+    			else:
+    				vals['code'] = str(int(parent_acc.code)+1000)
+    	else:
+    		accounts_last_id = self.env['account.account'].search([('parent_id', '=', False)], order='id desc', limit=1)
+    		if not accounts_last_id:
+    			vals['code'] = str(10000000)
+    		else:
+    			vals['code'] = str(int(accounts_last_id.code)+10000000)
+    	return super(AccountAccountInherit, self).create(vals)
